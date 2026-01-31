8777749f5d37f4063bbef3adde2ff13a4ae78f35
Oliviero Taleghani <o.taleghani@talesign.com>
2026-01-31 14:18:25 +0100

    added: fix

--- modules/hardware/settings.nix
+++ modules/hardware/settings.nix
@@ -9,9 +9,9 @@
 
     # Save space by optimizing the store automatically
     auto-optimise-store = lib.mkDefault true;
-
-    # Allow unfree packages (like Spotify, Discord, Nvidia drivers)
-    # This is almost always desired in a desktop distro
-    allowUnfree = lib.mkDefault true;
   };
+
+  # Allow unfree packages (like Spotify, Discord, Nvidia drivers)
+  # This is almost always desired in a desktop distro
+  nixpkgs.config.allowUnfree = lib.mkDefault true;
 }
