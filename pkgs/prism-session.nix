cecb462f6925114741f3f8a1e4dbddf5b70e9e30
Oliviero Taleghani <o.taleghani@talesign.com>
2026-02-02 08:33:12 +0100

    fix: rofi command

--- pkgs/prism-session.nix
+++ pkgs/prism-session.nix
@@ -18,7 +18,7 @@ writeShellScriptBin "prism-session" ''
   # Rofi Command
   # --dmenu: Reads from stdin
   # --placeholder: Sets the text in the search bar
-  selected_option=$(echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi --dmenu --placeholder "Session")
+  selected_option=$(echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi -show dmenu)
 
   case "$selected_option" in
     "$logout")
